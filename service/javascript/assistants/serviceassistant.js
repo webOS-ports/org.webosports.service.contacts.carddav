b73214bb8e9bb483cd07a739403895097ffdaae9
Achim Königs <garfonso@tratschtante.de>
2013-08-11 10:37:34 +0200

    removed logging of password from serviceAssitant

--- service/javascript/assistants/serviceassistant.js
+++ service/javascript/assistants/serviceassistant.js
@@ -16,13 +16,13 @@ var ServiceAssistant = Transport.ServiceAssistantBuilder({
 		setup: function setup(service, accountid, launchConfig, launchArgs) {
 			log("\n\n**************************START SERVICEASSISTANT*****************************");
 			//for testing only - will expose credentials to log file if left open
-			log("\n------------------->accountId:"+JSON.stringify(accountid));
-			log("\n------------------->launchConfig"+JSON.stringify(launchConfig));
-			log("\n------------------->launchArgs"+JSON.stringify(launchArgs));
-			log("\n------------------->service"+service);
+			//debug("\n------------------->accountId:"+JSON.stringify(accountid));
+			//debug("\n------------------->launchConfig"+JSON.stringify(launchConfig));
+			//debug"\n------------------->launchArgs"+JSON.stringify(launchArgs));
+			log("Starting " + launchConfig.name + " for account " + launchArgs.accountId + " from activity " + JSON.stringify(launchArgs.$activity));
 	
 			//this seems necessary for super class constructor during checkCredentials calls.
-			this.accountId = "";
+			this.accountId = launchArgs.accountId || "";
 			
 			//in onCreate call we will store config away in transport object. First store it in this, later on will be put into transport.
 			if (launchArgs.config && launchArgs.config.isModified) {
@@ -39,18 +39,18 @@ var ServiceAssistant = Transport.ServiceAssistantBuilder({
 			
 			future.then(function() {
 				var result = future.result;
-				log("iCal init came back.");
+				debug("iCal init came back.");
 				if (result.iCal) {
-					log("iCal init ok.");
+					debug("iCal init ok.");
 				}
 				future.nest(vCard.initialize());
 			});
 						
 			future.then(function() {
 				var result = future.result;
-				log("vCard init came back.");
+				debug("vCard init came back.");
 				if (result.vCard) {
-					log("vCard init ok.");
+					debug("vCard init ok.");
 				}
 				future.result = { returnValue: true };
 			});
@@ -61,29 +61,27 @@ var ServiceAssistant = Transport.ServiceAssistantBuilder({
 				if (launchConfig.name === "onDelete" || launchConfig.name === "checkCredentials") {
 					this.userAuth = {"username":launchArgs.username, "password":launchArgs.password, url: launchArgs.url};
 					future.result = {}; //do continue future execution
-				} else {
-					this.accountId = launchArgs.accountId || "";
-					
+				} else {					
 					future.nest(KeyStore.checkKey(this.accountId));
 					future.then(this, function() {
-						log("------------->Checked Key"+JSON.stringify(future.result));
+						debug("------------->Checked Key"+JSON.stringify(future.result));
 
 						if(future.result.value) {  //found key
-							log("------------->Existing Key Found");
+							debug("------------->Existing Key Found");
 							KeyStore.getKey(this.accountId).then(this, function(getKey) {
-								log("------------->Got Key: "+JSON.stringify(getKey.result));
+								//log("------------->Got Key: "+JSON.stringify(getKey.result));
 								this.userAuth = {"user":getKey.result.credentials.user, "password":getKey.result.credentials.password, "authToken": getKey.result.credentials.authToken};            
 							});
 						} else { //no key found - check for username / password and save
-							log("------------->No Key Found - Putting Key Data and storing globally");
+							debug("------------->No Key Found - Putting Key Data and storing globally");
 							if (launchArgs.config && launchArgs.config.credentials) {
 								var authToken = "Basic " + Base64.encode(launchArgs.config.credentials.user + ":" + launchArgs.config.credentials.password);
 								this.userAuth = {"user":launchArgs.config.credentials.user, "password":launchArgs.config.credentials.password, "authToken": authToken};
 								KeyStore.putKey(this.accountId, this.userAuth).then(function(putKey) {
-									log("------------->Saved Key"+JSON.stringify(putKey.result));
+									debug("------------->Saved Key"+JSON.stringify(putKey.result));
 								});
 							} else {
-								log("---->No config, can't do anything.");
+								debug("---->No config, can't do anything.");
 							}
 						}
 						future.result = { returnValue: true }; //continue with future execution.
@@ -94,7 +92,7 @@ var ServiceAssistant = Transport.ServiceAssistantBuilder({
 			
 			//preconfiguration of the service is complete...launch the sync engine
 			future.then(this, function() {
-				log("Calling super function  from future.then");
+				debug("Calling super function from future.then");
 				this.$super(setup)(service, this.accountId, undefined, Transport.HandlerFactoryBuilder(Sync.SyncHandler(Kinds)));
 				return true;
 			});
@@ -113,7 +111,7 @@ var ServiceAssistant = Transport.ServiceAssistantBuilder({
 });
 
 //these endpoints are delegated to the sync framework to handle - use the serviceassistant code above to intercept
-//var OnCreate = Sync.CreateAccountCommand;
+//var OnCreate = Sync.CreateAccountCommand; => now in own assistant. Does discovery.
 var OnDelete = Sync.DeleteAccountCommand;
 var OnEnabled = Sync.EnabledAccountCommand;
 
