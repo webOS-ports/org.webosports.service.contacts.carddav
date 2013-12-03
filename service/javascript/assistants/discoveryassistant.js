6f1af97716011a05444b3cc5dbd146396dfcd2b7
Achim Königs <garfonso@tratschtante.de>
2013-12-03 12:01:03 +0100

    discoveryAssistant will now work with new DB only
    this also makes parameters for discovery assistant much easier.
    Now only accountId is really required. The other ones can be used
    to overwrite account config stuff.

--- service/javascript/assistants/discoveryassistant.js
+++ service/javascript/assistants/discoveryassistant.js
@@ -1,22 +1,28 @@
 /*jslint sloppy: true, node: true, nomen: true */
-/*global Future, log, Kinds, debug, DB, CalDav, getTransportObjByAccountId */
+/*global Future, log, Kinds, debug, DB, CalDav, searchAccountConfig, getTransportObjByAccountId */
 
 var DiscoveryAssistant = function () {};
 
+//call with accountId (required), username, name, url.
+//accountId, name and username will be used to search for config object.
+//url will be updated in DB. If no object in DB, yet, URL is required.
 DiscoveryAssistant.prototype.run = function (outerFuture) {
 	var future = new Future(), args = this.controller.args;
 
 	//can only process the account we got credentials for => get right transport object
-	future.nest(getTransportObjByAccountId(args));
+	future.nest(searchAccountConfig(args));
 
 	future.then(this, function gotDBObject() {
-		var result = future.result;
+		var result = future.result || future.exception;
 		if (result.returnValue) {
-			future.nest(this.processAccount(args, result.account));
+			future.nest(this.processAccount(args, result.config));
 		} else {
 			log("Could not get DB object: " + JSON.stringify(result));
 			log(JSON.stringify(future.error));
-			future.result = {returnValue: false, success: false};
+			future.nest(this.processAccount(args, {
+				_kind: "org.webosports.service.contacts.carddav.account.config:1",
+				accountId: args.accountId
+			}));
 		}
 	});
 
@@ -28,35 +34,36 @@ DiscoveryAssistant.prototype.run = function (outerFuture) {
 	return outerFuture;
 };
 
-DiscoveryAssistant.prototype.processAccount = function (args, obj) {
-	var future = new Future(), params;
+DiscoveryAssistant.prototype.processAccount = function (args, config) {
+	var future = new Future(), outerFuture = new Future(), params;
 
-	if (obj) {
-		debug("Got transport object: " + JSON.stringify(obj));
+	if (config) {
+		debug("Got config object: " + JSON.stringify(config));
 
-		if (args[obj._id]) {
-			obj.config = {
-				name: args[obj._id].name,
-				url: args[obj._id].url
-			};
+		if (args.username) {
+			config.username = args.username;
+		}
+		if (args.name) {
+			config.name = args.name;
+		}
+		if (args.url) {
+			config.url = args.url;
 		}
 
-		if (!obj.config || !obj.config.url) {
-			log("No url for " + obj._id + " found in db or agruments. Can't process this account.");
+		if (config.url) {
+			log("No url for " + JSON.stringify(config) + " found in db or agruments. Can't process this account.");
 			future.reslut = {returnValue: false, success: false, msg: "No url for account in config."};
 			return future;
 		}
 
 		params = {
-			path: CalDav.setHostAndPort(obj.config.url),
+			path: CalDav.setHostAndPort(config.url),
 			authToken: this.client.userAuth.authToken,
-			originalUrl: obj.config.url
+			originalUrl: config.url
 		};
 
-		future.nest(CalDav.discovery(params));
-
 		future.then(this, function discoverCB() {
-			var result = future.result, i, f, config = obj.config, syncKey = obj.syncKey || {};
+			var result = future.result, i, f;
 
 			if (result.returnValue === true) {
 				config[Kinds.objects.calendarevent.name] = {
@@ -65,34 +72,6 @@ DiscoveryAssistant.prototype.processAccount = function (args, obj) {
 				config[Kinds.objects.contact.name] = {
 					homeFolder: result.contactHome
 				};
-
-				for (i in Kinds.objects) {
-					if (Kinds.objects.hasOwnProperty(i)) {
-						syncKey[Kinds.objects[i].name] = {
-							folders: []
-						};
-					}
-				}
-
-				for (i = 0; i < result.folders.length; i += 1) {
-					f = result.folders[i];
-					switch (f.resource) {
-					case "calendar":
-						syncKey[Kinds.objects.calendarevent.name].folders.push({uri: f.uri, name: f.name, remoteId: f.uri});
-						break;
-					case "contact":
-						syncKey[Kinds.objects.contact.name].folders.push({uri: f.uri, name: f.name, remoteId: f.uri});
-						break;
-					case "task":
-						syncKey[Kinds.objects.task.name].folders.push({uri: f.uri, name: f.name, remoteId: f.uri});
-						break;
-					default:
-						log("Discovery confused. Don't know resource type " + f.resource);
-						break;
-					}
-				}
-
-				obj.syncKey = syncKey;
 			} else {
 				log("Could not discover addressbook and calendar folders: " + JSON.stringify(result));
 
@@ -105,15 +84,15 @@ DiscoveryAssistant.prototype.processAccount = function (args, obj) {
 				};
 			}
 
-			future.nest(DB.merge([obj]));
+			future.nest(DB.merge([config]));
 		});
 
-		future.then(this, function storeCB() {
-			var result = future.result;
-			debug("Store came back: " + JSON.stringify(result));
-			future.result = {returnValue: result.returnValue, success: result.returnValue, config: obj.config, syncKey: obj.syncKey};
+		future.then(this, function storeConfigCB() {
+			var result = future.result || future.exception;
+			debug("Store config came back: " + JSON.stringify(result));
+			outerFuture.result = {returnValue: result.returnValue, success: result.returnValue, config: config};
 		});
 	}
 
-	return future;
+	return outerFuture;
 };
