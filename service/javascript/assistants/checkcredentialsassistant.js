c69dfddf837cd8c52a1e5c79883800eb5176994e
Achim Königs <garfonso@tratschtante.de>
2013-12-04 18:52:58 +0100

    reworked control flow in checkCredentialsAssistant
    - if credentials are changed the encrypted stuff is modified
    - and also the account.config object is updated
    - the assistant now blocks until everything is done
    - results are returned in all error cases

--- service/javascript/assistants/checkcredentialsassistant.js
+++ service/javascript/assistants/checkcredentialsassistant.js
@@ -1,12 +1,12 @@
 /*jslint sloppy: true, node: true, nomen: true */
-/*global debug, Base64, CalDav, DB, searchAccountConfig, Future, log */
+/*global debug, Base64, CalDav, DB, searchAccountConfig, Future, log, KeyStore */
 
 /* Validate contact username/password */
 var checkCredentialsAssistant = function () {};
 
 checkCredentialsAssistant.prototype.run = function (outerfuture) {
 	var args = this.controller.args, base64Auth, future = new Future();
-	debug("Account args =" + JSON.stringify(args));
+	//debug("Account args =" + JSON.stringify(args));
 
 	// Base64 encode username and password
 	base64Auth = "Basic " + Base64.encode(args.username + ":" + args.password);
@@ -18,17 +18,35 @@ checkCredentialsAssistant.prototype.run = function (outerfuture) {
 		future.result = {returnValue: true, obj: { config: {url: args.url}}};
 	}
 
+	//build result and send it back to UI.
+	function buildResult() {
+		outerfuture.result = {
+			success: true,
+			credentials: {
+				common: {
+					password: args.password,
+					username: args.username,
+					url: args.url
+				}
+			},
+			config: {
+				password: args.password,
+				username: args.username,
+				url: args.url
+			}
+		};
+	}
+
 	future.then(this, function gotConfigObject() {
-		var result = future.result, path, config;
-		debug("Result: " + JSON.stringify(result));
+		var result = future.result, path;
 		if (result.returnValue === true) {
-			config = result.config;
+			this.config = result.config;
 		}
 		if (args.url) {
 			path = CalDav.setHostAndPort(args.url);
 		} else {
-			if (config && config.url) {
-				path = CalDav.setHostAndPort(config.url);
+			if (this.config && this.config.url) {
+				path = CalDav.setHostAndPort(this.config.url);
 			} else {
 				log("No URL. Can't check credentials!");
 				outerfuture.result = {success: false, returnValue: false};
@@ -38,54 +56,60 @@ checkCredentialsAssistant.prototype.run = function (outerfuture) {
 
 		// Test basic authentication. If this fails username and or password is wrong
 		future.nest(CalDav.checkCredentials({authToken: base64Auth, path: path}));
+	});
 
-		future.then(function () {
-			var result = future.result, config;
-			// Check if we are getting a good return code for success
-			if (result.returnValue === true) {
-				// Pass back credentials and config (username/password/url); 
-				// config is passed to onCreate where
-				// we will save username/password in encrypted storage
-				debug("Password accepted");
-				
-				if (args.accountId && config) {
-					log("Had account id => this is change credentials call, update config object");
-					config.accountId = args.accountId || config.accountId;
-					config.name = args.name || config.name;
-					config.username = args.username || config.username;
-					config.url = args.url || config.url;
-					
-					if (config._id && config._kind) {
-						DB.merge([config]).then(function mergeCB() {
-							var result = future.result || future.exception;
-							log("Stored config in config db: " + JSON.stringify(result));
-						});
-					} else {
-						log("Did not have config object in DB. Won't put new one to prevent duplicates.");
-					}
-				}
+	future.then(this, function credentialsCheckCB() {
+		var result = future.result, authToken;
+		// Check if we are getting a good return code for success
+		if (result.returnValue === true) {
+			// Pass back credentials and config (username/password/url);
+			// config is passed to onCreate where
+			// we will save username/password in encrypted storage
+			debug("Password accepted");
 
-				//send results back to UI:
-				outerfuture.result = {
-					success: true,
-					credentials: {
-						common: {
-							password: args.password,
-							username: args.username,
-							url: args.url
-						}
-					},
-					config: {
-						password: args.password,
-						username: args.username,
-						url: args.url
-					}
-				};
+			if (args.accountId) {
+				log("Had account id => this is change credentials call, update config object");
+				authToken = "Basic " + Base64.encode(args.username + ":" + args.password);
+				this.client.userAuth = {"user": args.username, "password": args.password, "authToken": authToken};
+
+				future.nest(KeyStore.putKey(args.accountId, this.client.userAuth));
 			} else {
-				debug("Password rejected");
-				outerfuture.result = {success: false};
+			//send results back to UI:
+				buildResult();
 			}
-		});
+
+		} else {
+			debug("Password rejected");
+			outerfuture.result = {returnValue: false, success: false};
+		}
+	});
+
+	future.then(this, function updateCredentialsCB() {
+		var result = future.result || future.exception;
+		debug("------------->Modified Key: " + JSON.stringify(result));
+
+		if (this.config) {
+			this.config.accountId = args.accountId || this.config.accountId;
+			this.config.name = args.name || this.config.name;
+			this.config.username = args.username || this.config.username;
+			this.config.url = args.url || this.config.url;
+
+			if (this.config._id && this.config._kind) {
+				future.nest(DB.merge([this.config]));
+			} else {
+				log("Did not have config object in DB. Won't put new one to prevent duplicates.");
+				buildResult();
+			}
+		} else {
+			log("No config found => can't save it.");
+			buildResult();
+		}
+	});
+
+	future.then(this, function mergeCB() {
+		var result = future.result || future.exception;
+		log("Stored config in config db: " + JSON.stringify(result));
+		buildResult();
 	});
 
 	return outerfuture;
