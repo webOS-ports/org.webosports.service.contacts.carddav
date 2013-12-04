19d59c3a23cf12364ce270ebd2719f3c67f3e7ad
Achim Königs <garfonso@tratschtante.de>
2013-12-04 19:00:51 +0100

    fixed some nasty bugs in accountConfigUtils.js

--- service/javascript/utils/accountConfigUtils.js
+++ service/javascript/utils/accountConfigUtils.js
@@ -27,14 +27,20 @@ var getTransportObjByAccountId = function (args) {
 	}
 
 	future.then(this, function gotDBObject() {
-		var result = future.result, i, obj;
+		var result = future.result || future.exception, i, obj, found = false;
 		if (result.returnValue) {
 			for (i = 0; i < result.results.length; i += 1) {
 				obj = result.results[i];
 				if (obj.accountId === args.accountId) {
 					future.result = {returnValue: true, transportObj: obj};
+					found = true;
 					break;
 				}
+			}
+
+			if (!found) {
+				log("No transport object for account " + args.accountId);
+				future.result = {returnValue: false, success: false};
 			}
 		} else {
 			log("Could not get DB object: " + JSON.stringify(result));
@@ -48,11 +54,11 @@ var getTransportObjByAccountId = function (args) {
 
 //recursive method to search in config db. Will return a future that get's the config object as result.
 var searchAccountConfigInConfigDB = function (config, param, next, nextNext) {
-	var future, outerFuture;
+	var future = new Future(), outerFuture = new Future();
 	if (!param) { //exit condition.
 		log("Could not find any information about account " + config.accountId + " in config db.");
 		outerFuture.result = {returnValue: false }; //continue execution.
-		return;
+		return outerFuture;
 	}
 
 	if (config[param]) {
@@ -63,35 +69,37 @@ var searchAccountConfigInConfigDB = function (config, param, next, nextNext) {
 
 		future.then(function searchCB() {
 			try {
-				var result = future.result();
-				if (result.returnValue === true) {
-					if (result.results.length > 0 && result.results[0].config) {
+				var result = future.result || future.exception;
+				if (result.returnValue) {
+					if (result.results.length > 0) {
+						delete result.results[0]._rev;
 						outerFuture.result = {
-							config: result.results[0].config,
+							config: result.results[0],
 							returnValue: true
 						};
-						log("Found config with " + param + ": " + JSON.stringify(config));
+						//log("Found config with " + param + ": " + JSON.stringify(config));
 
 						if (result.results.length > 1) {
 							log("WARNING: Found multiple results for account!!");
 						}
 					} else {
-						searchAccountConfigInConfigDB(next, nextNext); //try next one.
+						//log("No config object for " + param + " = " + config[param]);
+						outerFuture.nest(searchAccountConfigInConfigDB(config, next, nextNext)); //try next one.
 					}
 				} else {
-					result = future.exception;
 					log("Could not find with param " + param + ". Reason: " + result.message);
-					searchAccountConfigInConfigDB(next, nextNext); //try next one.
+					outerFuture.nest(searchAccountConfigInConfigDB(config, next, nextNext)); //try next one.
 				}
 			} catch (e) {
 				log("Got exception while find with param " + param + ". Message: " + e.message);
-				searchAccountConfigInConfigDB(next, nextNext); //try next one.
+				outerFuture.nest(searchAccountConfigInConfigDB(config, next, nextNext)); //try next one.
 			}
 		});
 	} else {
-		debug("No value for " + param + " in service config.");
-		searchAccountConfigInConfigDB(next, nextNext); //try next one.
+		//debug("No value for " + param + " in service config.");
+		outerFuture.nest(searchAccountConfigInConfigDB(config, next, nextNext)); //try next one.
 	}
+
 	return outerFuture;
 };
 
@@ -105,35 +113,41 @@ var searchAccountConfig = function (args) {
 	future.then(function configCB() {
 		var result = future.result || future.exception;
 		if (result.returnValue === true) {
+			//log("Found config in config db: " + JSON.stringify(result.config));
 			outerFuture.result = { returnValue: true, config: result.config };
-			future.result = { returnValue: false }; //stop search.
 		} else {
+			log("Did not find object in config db, try transport object.");
 			future.nest(getTransportObjByAccountId(args));
 		}
 	});
 
 	future.then(function transportCB() {
 		var result = future.result || future.exception, config;
+
 		if (result.returnValue === true) {
 			if (result.transportObj && result.transportObj.config) {
-				outerFuture.result = { returnValue: true, config: result.transportObj.config };
-
 				log("Transfering old config store into new one.");
 				config = result.transportObj.config;
 				config.username = args.username || config.username;
-				config.accountId = args.username || config.accountId;
+				config.accountId = args.accountId || config.accountId;
 				config.name = args.name || config.name;
 				config._kind = "org.webosports.service.contacts.carddav.account.config:1";
 				delete config._id;
 				DB.merge([config]).then(function mergeCB(f) {
 					var result = f.result || f.exception;
 					log("Config store came back: " + JSON.stringify(result));
+					if (result.returnValue === true) {
+						config._id = result.results[0].id;
+					}
+					outerFuture.result = { returnValue: true, config: config };
 				});
 			} else {
 				//no config in transport object => fail.
+				//log("No transport object for account.");
 				outerFuture.result = { returnValue: false };
 			}
 		} else {
+			log("Failed to get transport object for account.");
 			outerFuture.result = { returnValue: false };
 		}
 	});
