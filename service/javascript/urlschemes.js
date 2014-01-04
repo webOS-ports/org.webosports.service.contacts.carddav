026609cc995196c72cd654db5ad00f9447bacacd
Achim Königs <garfonso@tratschtante.de>
2014-01-04 18:22:34 +0100

    improved url schemes to allow multiple keys
    this allows us to support owncloud also if it is installed in the root,
    like in cloudu. :)

--- service/javascript/urlschemes.js
+++ service/javascript/urlschemes.js
@@ -8,35 +8,35 @@ var UrlSchemes = {
 	//	   if a replacement is required more than once, change function!
 	urlSchemes: [
 		{
-			keys:			  "DISABLEDicloud.com",
+			keys:			  ["DISABLEDicloud.com"],
 			calendar:	      "https://p02-caldav.icloud.com:443",
 			contact:		  "https://p02-contacts.icloud.com:443",
 			checkCredentials: "https://p02-contacts.icloud.com:443"
 		},
 		{
-			key:			  "google.",
+			keys:			  ["google."],
 			calendar:	      "https://www.google.com:443/calendar/dav/%USERNAME%/user/",
 			contact:		  "https://www.google.com:443/carddav/v1/principals/%USERNAME/lists/",
 			checkCredentials: "https://www.google.com:443/calendar/dav/%USERNAME%/user"
 		},
 		{
-			key:	          "DISABLEDyahoo.",
+			keys:	          ["DISABLEDyahoo."],
 			calendar:	      "https://caldav.calendar.yahoo.com",
 			contact:		  "https://carddav.address.yahoo.com",
 			checkCredentials: "https://carddav.address.yahoo.com"
 		},
 		{
-			key:			  "/owncloud",
+			keys:			  ["/owncloud", "cloudu.de"],
             //issue: calendar/contact contain display name, which we don't know and can be different from username??
-			calendar:	      "%URL_PREFIX%/owncloud/remote.php/caldav/calendars/%USERNAME%/",
-			contact:		  "%URL_PREFIX%/owncloud/remote.php/carddav/addressbooks/%USERNAME%/",
-			checkCredentials: "%URL_PREFIX%/owncloud/remote.php/caldav"
+			calendar:	      "%URL_PREFIX%/remote.php/caldav/calendars/%USERNAME%/",
+			contact:		  "%URL_PREFIX%/remote.php/carddav/addressbooks/%USERNAME%/",
+			checkCredentials: "%URL_PREFIX%/remote.php/caldav"
 		},
 		{
-			key:			  "/egroupware",
-			calendar:	      "%URL_PREFIX%/egroupware/groupdav.php/%USERNAME%/",
-			contact:		  "%URL_PREFIX%/egroupware/groupdav.php/%USERNAME%/",
-			checkCredentials: "%URL_PREFIX%/egroupware/groupdav.php"
+			keys:			  ["/egroupware"],
+			calendar:	      "%URL_PREFIX%/groupdav.php/%USERNAME%/",
+			contact:		  "%URL_PREFIX%/groupdav.php/%USERNAME%/",
+			checkCredentials: "%URL_PREFIX%/groupdav.php"
 		}
 	],
 
@@ -46,7 +46,10 @@ var UrlSchemes = {
 
 	resolveURL: function (url, username, type) {
 		"use strict";
-		var i, scheme, index, prefix, newURL;
+		var i, j, scheme, index, prefix, newURL;
+		url = url.toLowerCase();
+		debug("Resolving " + url);
+
 		if (this.cache[url] &&  this.cache[url][type]) {
 			debug("Returning cached url ", this.cache[url][type]);
 			return this.cache[url][type];
@@ -58,16 +61,18 @@ var UrlSchemes = {
 
 		for (i = 0; i < this.urlSchemes.length; i += 1) {
 			scheme = this.urlSchemes[i];
-			index = url.indexOf(scheme.key);
-			if (index >= 0) {
-				debug("Found URL for scheme ", scheme);
-				prefix = url.substring(0, index);
-				debug("Prefix: ", prefix);
-				newURL = scheme[type].replace("%URL_PREFIX%", prefix);
-				newURL = newURL.replace("%USERNAME%", username); //This will only replace once.
-				debug("Returning new URL: ", newURL);
-				this.cache[url][type] = newURL;
-				return newURL;
+			for (j = 0; j < scheme.keys.length; j += 1) {
+				index = url.indexOf(scheme.keys[j]);
+				if (index >= 0) {
+					debug("Found URL for scheme ", scheme);
+					prefix = url.substring(0, index + scheme.keys[j].length);
+					debug("Prefix: ", prefix);
+					newURL = scheme[type].replace("%URL_PREFIX%", prefix);
+					newURL = newURL.replace("%USERNAME%", username); //This will only replace once.
+					debug("Returning new URL: ", newURL);
+					this.cache[url][type] = newURL;
+					return newURL;
+				}
 			}
 		}
 
