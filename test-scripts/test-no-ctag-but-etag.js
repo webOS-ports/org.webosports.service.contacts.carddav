3592fb169be35475ff400652838831b04d4512f1
Achim Königs <garfonso@tratschtante.de>
2014-09-28 23:11:00 +0200

    added test for etag instead of ctag for folder
    Signed-off-by: Achim Königs <garfonso@mobo.info>

---
+++ test-scripts/test-no-ctag-but-etag.js
@@ -0,0 +1,21 @@
+/*global require, console, global, Future */
+
+var fs = require("fs");
+var path = fs.realpathSync(".");
+global.servicePath = path + "/../service";
+global.checkResult = require("./../service/javascript/utils/checkResult");
+global.Log = require("./../service/javascript/utils/Log.js");
+global.Future = require("./foundations/Future");
+var CalDav = require("./../service/javascript/utils/CalDav.js");
+
+global.Log.setFilename("test-no-ctag-but-etag.txt");
+
+global.httpClient = require("./mock.js").httpClient;
+global.httpClient.filename = "test-no-ctag-but-etag.xml";
+
+var future = CalDav.checkForChanges({userAuth: {}, path: "/"});
+
+future.then(function () {
+    "use strict";
+    console.log("CheckForChanges result: ", future.result);
+});
