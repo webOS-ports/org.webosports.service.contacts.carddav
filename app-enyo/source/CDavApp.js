e22f0a015e270083c588fc307d6ff167131a4fe7
Achim Königs <garfonso@tratschtante.de>
2015-02-12 10:55:46 +0100

    app: white space changes.

--- app-enyo/source/CDavApp.js
+++ app-enyo/source/CDavApp.js
@@ -2,114 +2,114 @@
 /*global enyo, $L, console, setTimeout, PalmSystem */
 
 function log(msg) {
-    console.error(msg);
+	console.error(msg);
 }
 
 function debug(msg) {
-    console.error(msg);
+	console.error(msg);
 }
 
 enyo.kind({
-    name: "Main.CDavApp",
-    width: "100%",
-    kind: "VFlexBox",
-    className: "enyo-bg",
-    components: [
-        { name: "discovery", kind: "PalmService", service: "palm://org.webosports.cdav.service/",
-            method: "discovery", onSuccess: "cdavOK", onFailure: "cdavFailed" },
-        { name: "sync", kind: "PalmService", service: "palm://org.webosports.cdav.service/",
-            method: "sync", onSuccess: "cdavOK", onFailure: "cdavFailed" },
-        { name: "triggerSlowSync", kind: "PalmService", service: "palm://org.webosports.cdav.service/",
-            method: "triggerSlowSync", onSuccess: "cdavOK", onFailure: "cdavFailed" },
-        { name: "dbAccounts", kind: "DbService", dbKind: "org.webosports.cdav.account.config:1", onFailure: "cdavFailed", components: [
-            { name: "findAccounts", method: "find", onSuccess: "refreshAccounts" }
-        ]},
-        { kind: "PageHeader", content: "C+Dav Application", pack: "center" },
-        { kind: "Scroller", flex: 1, style: "margin:30px;", components: [
-            { name: "alert", style: "margin-bottom:30px;text-align:center; background-color:red; color:yellow;" },
-            { name: "success", style: "margin-bottom:30px;text-align:center; background-color:green; color:yellow;" },
-            { kind: "RowGroup", caption: "Connection settings", components: [
-                { kind: "Picker", label: "Account: "},
-                { kind: "Button", tabIndex: "1",  caption: "Trigger Slow Sync", onclick: "doTriggerSlowSync", className: "enyo-button-dark" },
-                { kind: "Button", tabIndex: "2",  caption: "Do Auto Discovery", onclick: "doDiscovery", className: "enyo-button-dark" },
-                { kind: "Button", tabIndex: "3",  caption: "Start Sync", onclick: "doSync", className: "enyo-button-dark" }
-            ]}
-        ]},
-        {className: "accounts-footer-shadow", tabIndex: -1}
-    ],
-    create: function () {
-        this.inherited(arguments);
+	name: "Main.CDavApp",
+	width: "100%",
+	kind: "VFlexBox",
+	className: "enyo-bg",
+	components: [
+		{ name: "discovery", kind: "PalmService", service: "palm://org.webosports.cdav.service/",
+			method: "discovery", onSuccess: "cdavOK", onFailure: "cdavFailed" },
+		{ name: "sync", kind: "PalmService", service: "palm://org.webosports.cdav.service/",
+			method: "sync", onSuccess: "cdavOK", onFailure: "cdavFailed" },
+		{ name: "triggerSlowSync", kind: "PalmService", service: "palm://org.webosports.cdav.service/",
+			method: "triggerSlowSync", onSuccess: "cdavOK", onFailure: "cdavFailed" },
+		{ name: "dbAccounts", kind: "DbService", dbKind: "org.webosports.cdav.account.config:1", onFailure: "cdavFailed", components: [
+			{ name: "findAccounts", method: "find", onSuccess: "refreshAccounts" }
+		]},
+		{ kind: "PageHeader", content: "C+Dav Application", pack: "center" },
+		{ kind: "Scroller", flex: 1, style: "margin:30px;", components: [
+			{ name: "alert", style: "margin-bottom:30px;text-align:center; background-color:red; color:yellow;" },
+			{ name: "success", style: "margin-bottom:30px;text-align:center; background-color:green; color:yellow;" },
+			{ kind: "RowGroup", caption: "Connection settings", components: [
+				{ kind: "Picker", label: "Account: "},
+				{ kind: "Button", tabIndex: "1",  caption: "Trigger Slow Sync", onclick: "doTriggerSlowSync", className: "enyo-button-dark" },
+				{ kind: "Button", tabIndex: "2",  caption: "Do Auto Discovery", onclick: "doDiscovery", className: "enyo-button-dark" },
+				{ kind: "Button", tabIndex: "3",  caption: "Start Sync", onclick: "doSync", className: "enyo-button-dark" }
+			]}
+		]},
+		{className: "accounts-footer-shadow", tabIndex: -1}
+	],
+	create: function () {
+		this.inherited(arguments);
 
-        this.$.findAccounts.call({query: {from: "org.webosports.cdav.account.config:1"}});
-    },
-    refreshAccounts: function (inSender, inResponse) {
-        debug("Response from db: " + JSON.stringify(inResponse));
+		this.$.findAccounts.call({query: {from: "org.webosports.cdav.account.config:1"}});
+	},
+	refreshAccounts: function (inSender, inResponse) {
+		debug("Response from db: " + JSON.stringify(inResponse));
 
-        this.accounts = inResponse.results;
-        var i, items = [];
-        for (i = 0; i < this.accounts.length; i += 1) {
-            items.push({caption: this.accounts[i].name, value: this.accounts[i].accountId});
-        }
-        this.$.picker.setItems(items);
-        this.$.picker.setValue(items[0].value);
-        this.$.picker.render();
-    },
-    showError: function (msg) {
-        debug("Error: " + msg);
-        this.$.success.setContent("");
-        this.$.alert.setContent(msg);
-    },
-    showSuccess: function (msg) {
-        debug("Success: " + msg);
-        this.$.alert.setContent("");
-        this.$.success.setContent(msg);
-    },
-    doTriggerSlowSync: function () {
-        var accountId = this.$.picker.getValue();
+		this.accounts = inResponse.results;
+		var i, items = [];
+		for (i = 0; i < this.accounts.length; i += 1) {
+			items.push({caption: this.accounts[i].name, value: this.accounts[i].accountId});
+		}
+		this.$.picker.setItems(items);
+		this.$.picker.setValue(items[0].value);
+		this.$.picker.render();
+	},
+	showError: function (msg) {
+		debug("Error: " + msg);
+		this.$.success.setContent("");
+		this.$.alert.setContent(msg);
+	},
+	showSuccess: function (msg) {
+		debug("Success: " + msg);
+		this.$.alert.setContent("");
+		this.$.success.setContent(msg);
+	},
+	doTriggerSlowSync: function () {
+		var accountId = this.$.picker.getValue();
 
-        debug("AccountId: " + JSON.stringify(accountId));
-        if (accountId) {
-            enyo.scrim.show();
-            this.showError("");
-            debug("Starting call...");
-            this.$.triggerSlowSync.call({
-                accountId: accountId
-            });
-            debug("... done... ");
-        }
-    },
-    doDiscovery: function () {
-        var accountId = this.$.picker.getValue();
+		debug("AccountId: " + JSON.stringify(accountId));
+		if (accountId) {
+			enyo.scrim.show();
+			this.showError("");
+			debug("Starting call...");
+			this.$.triggerSlowSync.call({
+				accountId: accountId
+			});
+			debug("... done... ");
+		}
+	},
+	doDiscovery: function () {
+		var accountId = this.$.picker.getValue();
 
-        if (accountId) {
-            enyo.scrim.show();
-            this.showError("");
-            this.$.discovery.call({
-                accountId: accountId
-            });
-        }
-    },
-    doSync: function () {
-        var accountId = this.$.picker.getValue();
+		if (accountId) {
+			enyo.scrim.show();
+			this.showError("");
+			this.$.discovery.call({
+				accountId: accountId
+			});
+		}
+	},
+	doSync: function () {
+		var accountId = this.$.picker.getValue();
 
-        if (accountId) {
-            enyo.scrim.show();
-            this.showError("");
-            this.$.sync.call({
-                accountId: accountId
-            });
-        }
-    },
-    cdavOK: function (inSender, inResponse) {
-        enyo.scrim.hide();
+		if (accountId) {
+			enyo.scrim.show();
+			this.showError("");
+			this.$.sync.call({
+				accountId: accountId
+			});
+		}
+	},
+	cdavOK: function (inSender, inResponse) {
+		enyo.scrim.hide();
 
-        debug("Success Response: " + JSON.stringify(inResponse));
-        this.showSuccess("Method success: " + JSON.stringify(inResponse));
-    },
-    cdavFailed: function (inSender, inResponse) {
-        enyo.scrim.hide();
+		debug("Success Response: " + JSON.stringify(inResponse));
+		this.showSuccess("Method success: " + JSON.stringify(inResponse));
+	},
+	cdavFailed: function (inSender, inResponse) {
+		enyo.scrim.hide();
 
-        debug("Error Response: " + JSON.stringify(inResponse));
-        this.showError("Method failure: " + JSON.stringify(inResponse));
-    }
+		debug("Error Response: " + JSON.stringify(inResponse));
+		this.showError("Method failure: " + JSON.stringify(inResponse));
+	}
 });
--- app/app/assistants/main-assistant.js
+++ app/app/assistants/main-assistant.js
@@ -1,143 +1,143 @@
 //JSLint options:
 /*global $L, Mojo, AppAssistant, console, PalmCall */
 function MainAssistant() {
-    "use strict";
+	"use strict";
 }
 
 MainAssistant.prototype.setup = function () {
-    "use strict";
-    this.triggerSlowModel = {label: $L("Trigger Slow Sync"), disabled: true};
-    this.discoveryModel = {label: $L("Do auto discovery"), disabled: true};
-    this.startSyncModel = {label: $L("Start sync"), disabled: true};
+	"use strict";
+	this.triggerSlowModel = {label: $L("Trigger Slow Sync"), disabled: true};
+	this.discoveryModel = {label: $L("Do auto discovery"), disabled: true};
+	this.startSyncModel = {label: $L("Start sync"), disabled: true};
 
-    //this.controller.setupWidget(Mojo.Menu.appMenu, {}, AppAssistant.prototype.MenuModel);
+	//this.controller.setupWidget(Mojo.Menu.appMenu, {}, AppAssistant.prototype.MenuModel);
 
-    /* setup widgets here */
-    this.controller.setupWidget("btnTriggerSlow", {}, this.triggerSlowModel);
-    this.controller.setupWidget("btnTriggerDiscovery", {}, this.discoveryModel);
-    this.controller.setupWidget("btnStartSync", {}, this.startSyncModel);
+	/* setup widgets here */
+	this.controller.setupWidget("btnTriggerSlow", {}, this.triggerSlowModel);
+	this.controller.setupWidget("btnTriggerDiscovery", {}, this.discoveryModel);
+	this.controller.setupWidget("btnStartSync", {}, this.startSyncModel);
 
-    this.dropboxModel = {choices: [], disabled: true };
-    this.dropBox = this.controller.setupWidget("lsAccounts", {label: $L("Account")}, this.dropboxModel);
+	this.dropboxModel = {choices: [], disabled: true };
+	this.dropBox = this.controller.setupWidget("lsAccounts", {label: $L("Account")}, this.dropboxModel);
 
-    this.spinnerModel = { spinning: true };
-    this.controller.setupWidget("loadSpinner", this.attributes = { spinnerSize: "large" }, this.spinnerModel);
+	this.spinnerModel = { spinning: true };
+	this.controller.setupWidget("loadSpinner", this.attributes = { spinnerSize: "large" }, this.spinnerModel);
 
-    /* add event handlers to listen to events from widgets */
-    Mojo.Event.listen(this.controller.get("btnTriggerSlow"), Mojo.Event.tap, this.triggerSlow.bind(this));
-    Mojo.Event.listen(this.controller.get("btnTriggerDiscovery"), Mojo.Event.tap, this.startDiscovery.bind(this));
-    Mojo.Event.listen(this.controller.get("btnStartSync"), Mojo.Event.tap, this.startSync.bind(this));
+	/* add event handlers to listen to events from widgets */
+	Mojo.Event.listen(this.controller.get("btnTriggerSlow"), Mojo.Event.tap, this.triggerSlow.bind(this));
+	Mojo.Event.listen(this.controller.get("btnTriggerDiscovery"), Mojo.Event.tap, this.startDiscovery.bind(this));
+	Mojo.Event.listen(this.controller.get("btnStartSync"), Mojo.Event.tap, this.startSync.bind(this));
 };
 
 MainAssistant.prototype.startSync = function (event) {
-    "use strict";
-    this.callService("sync");
+	"use strict";
+	this.callService("sync");
 };
 
 MainAssistant.prototype.startDiscovery = function (event) {
-    "use strict";
-    this.callService("discovery");
+	"use strict";
+	this.callService("discovery");
 };
 
 MainAssistant.prototype.triggerSlow = function (event) {
-    "use strict";
-    this.callService("triggerSlowSync");
+	"use strict";
+	this.callService("triggerSlowSync");
 };
 
 MainAssistant.prototype.callService = function (method) {
-    "use strict";
-    this.setControlsEnabled(false);
-    console.error("Selecting account " + this.dropboxModel.value);
-    this.currentAccount = this.dropboxModel.value;
-    if (this.currentAccount !== -1) {
-        PalmCall.call("palm://org.webosports.cdav.service/",
-                      method,
-                      this.accounts[this.currentAccount]).then(this, function serviceCB(f) {
-            var result = f.result;
-            this.setControlsEnabled(true);
-            this.showMessage(method + " Result", method + " result: " + JSON.stringify(result));
-        });
-    } else {
-        this.showMessage($L("Error"), "You need to select an account first.");
-    }
+	"use strict";
+	this.setControlsEnabled(false);
+	console.error("Selecting account " + this.dropboxModel.value);
+	this.currentAccount = this.dropboxModel.value;
+	if (this.currentAccount !== -1) {
+		PalmCall.call("palm://org.webosports.cdav.service/",
+					  method,
+					  this.accounts[this.currentAccount]).then(this, function serviceCB(f) {
+			var result = f.result;
+			this.setControlsEnabled(true);
+			this.showMessage(method + " Result", method + " result: " + JSON.stringify(result));
+		});
+	} else {
+		this.showMessage($L("Error"), "You need to select an account first.");
+	}
 };
 
 MainAssistant.prototype.setControlsEnabled = function (enabled) {
-    "use strict";
-    this.triggerSlowModel.disabled = !enabled;
-    this.discoveryModel.disabled = !enabled;
-    this.startSyncModel.disabled = !enabled;
-    this.controller.modelChanged(this.triggerSlowModel);
-    this.controller.modelChanged(this.discoveryModel);
-    this.controller.modelChanged(this.startSyncModel);
-    if (enabled) {
-        this.controller.get('Scrim').hide();
-        this.controller.get('loadSpinner').mojo.stop();
-    } else {
-        this.controller.get('Scrim').show();
-        this.controller.get('loadSpinner').mojo.start();
-    }
+	"use strict";
+	this.triggerSlowModel.disabled = !enabled;
+	this.discoveryModel.disabled = !enabled;
+	this.startSyncModel.disabled = !enabled;
+	this.controller.modelChanged(this.triggerSlowModel);
+	this.controller.modelChanged(this.discoveryModel);
+	this.controller.modelChanged(this.startSyncModel);
+	if (enabled) {
+		this.controller.get('Scrim').hide();
+		this.controller.get('loadSpinner').mojo.stop();
+	} else {
+		this.controller.get('Scrim').show();
+		this.controller.get('loadSpinner').mojo.start();
+	}
 };
 
 MainAssistant.prototype.refreshAccounts = function () {
-    "use strict";
-    var i;
-    this.dropboxModel.choices = [];
-    for (i = 0; i < this.accounts.length; i += 1) {
-        console.error("Got account: " + this.accounts[i].name);
-        this.dropboxModel.choices.push({label: this.accounts[i].name, value: i});
-    }
+	"use strict";
+	var i;
+	this.dropboxModel.choices = [];
+	for (i = 0; i < this.accounts.length; i += 1) {
+		console.error("Got account: " + this.accounts[i].name);
+		this.dropboxModel.choices.push({label: this.accounts[i].name, value: i});
+	}
 
-    if (!this.dropboxModel.value || this.dropboxModel.value < 0 || this.dropboxModel.value >= this.accounts.length) {
-        this.dropboxModel.value = 0;
-    }
+	if (!this.dropboxModel.value || this.dropboxModel.value < 0 || this.dropboxModel.value >= this.accounts.length) {
+		this.dropboxModel.value = 0;
+	}
 
-    this.dropboxModel.disabled = false;
-    this.controller.modelChanged(this.dropboxModel);
-    this.setControlsEnabled(true);
+	this.dropboxModel.disabled = false;
+	this.controller.modelChanged(this.dropboxModel);
+	this.setControlsEnabled(true);
 };
 
 MainAssistant.prototype.activate = function (event) {
-    "use strict";
-    this.setControlsEnabled(false);
+	"use strict";
+	this.setControlsEnabled(false);
 
-    PalmCall.call("palm://com.palm.db/", "find",
-                  {query: {from: "org.webosports.cdav.account.config:1"}}).then(this, function (f) {
-        var result = f.result;
-        if (result.returnValue === true) {
-            console.error("Got accounts.");
-            this.accounts = f.result.results;
-            console.error("Now have " + this.accounts.length + " accounts.");
-            if (this.accounts.length > 0) {
-                this.currentAccount = 0;
-            }
-            this.refreshAccounts();
-            console.error("Ready to go.");
-        } else {
-            console.error("Could not get accounts..." + JSON.stringify(f.result));
-            this.showMessage("Error", "Could not get accounts. Error: " + JSON.stringify(f.result));
-        }
-    });
+	PalmCall.call("palm://com.palm.db/", "find",
+				  {query: {from: "org.webosports.cdav.account.config:1"}}).then(this, function (f) {
+		var result = f.result;
+		if (result.returnValue === true) {
+			console.error("Got accounts.");
+			this.accounts = f.result.results;
+			console.error("Now have " + this.accounts.length + " accounts.");
+			if (this.accounts.length > 0) {
+				this.currentAccount = 0;
+			}
+			this.refreshAccounts();
+			console.error("Ready to go.");
+		} else {
+			console.error("Could not get accounts..." + JSON.stringify(f.result));
+			this.showMessage("Error", "Could not get accounts. Error: " + JSON.stringify(f.result));
+		}
+	});
 };
 
 MainAssistant.prototype.showMessage = function (title, message) {
-    "use strict";
-    this.controller.showAlertDialog({
-        title: title,
-        message: message,
-        choices: [{label: $L("OK"), value: "OK"}]
-    });
+	"use strict";
+	this.controller.showAlertDialog({
+		title: title,
+		message: message,
+		choices: [{label: $L("OK"), value: "OK"}]
+	});
 };
 
 
 MainAssistant.prototype.deactivate = function (event) {
-    "use strict";
-    /* remove any event handlers you added in activate and do any other cleanup that should happen before
-       this scene is popped or another scene is pushed on top */
+	"use strict";
+	/* remove any event handlers you added in activate and do any other cleanup that should happen before
+	   this scene is popped or another scene is pushed on top */
 };
 
 MainAssistant.prototype.cleanup = function (event) {
-    "use strict";
-    /* this function should do any cleanup needed before the scene is destroyed as
-       a result of being popped off the scene stack */
+	"use strict";
+	/* this function should do any cleanup needed before the scene is destroyed as
+	   a result of being popped off the scene stack */
 };
