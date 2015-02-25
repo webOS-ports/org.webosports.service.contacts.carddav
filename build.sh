1f5462a97098041e7667ed9389d39c52b4d39077
Achim Königs <garfonso@tratschtante.de>
2015-02-25 12:59:42 +0100

    build.sh now uses node.js to build
    Signed-off-by: Achim Königs <garfonso@mobo.info>

--- build.sh
+++ build.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-RESULT=org.webosports.cdav_0.3.20_all
-
-rm *.ipk
-
-mv service/javascript/kinds.js service/javascript/kinds_no_upsync.js
-mv service/javascript/kinds_upsync.js service/javascript/kinds.js
-
-palm-package package app service accounts accounts-google-mojo accounts-icloud accounts-yahoo
-
-mv $RESULT".ipk" $RESULT"_upsync.ipk"
-
-palm-package package app-enyo service accounts-enyo accounts-google accounts-icloud accounts-yahoo
-
-mv $RESULT".ipk" $RESULT"_enyo_upsync.ipk"
-
-mv service/javascript/kinds.js service/javascript/kinds_upsync.js
-mv service/javascript/kinds_no_upsync.js service/javascript/kinds.js
-
-palm-package package app service accounts accounts-google-mojo accounts-icloud accounts-yahoo
-
-mv $RESULT".ipk" $RESULT"_no_upsync.ipk"
-
-palm-package package app-enyo service accounts-enyo accounts-google accounts-icloud accounts-yahoo
-
-mv $RESULT".ipk" $RESULT"_enyo_no_upsync.ipk"
-
-palm-install $RESULT"_upsync.ipk"
+node build.js $1
